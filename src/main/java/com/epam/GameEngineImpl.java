cd9901e3cd7ecc90c3e9f19b1560962ac3b37a13
Daniil_Smirnov <daniil_smirnov@epam.com>
2018-05-12 15:43:33 +0300

    tests added.

---
+++ src/main/java/com/epam/GameEngineImpl.java
@@ -0,0 +1,19 @@
+package com.epam;
+
+public class GameEngineImpl implements GameEngine {
+
+    @Override
+    public boolean[][] compute(boolean[][] initialState, int numberIterations) {
+        return new boolean[0][];
+    }
+
+    private byte[][] parse(boolean[][] array) {
+
+        return null;
+    }
+
+    private boolean[][] parse(byte[][] array) {
+
+        return null;
+    }
+}
